83483d458649cc7f9310820f0938d772a285bc5e
Jonathan Chiou <jchio799@gmail.com>
2018-11-16 19:15:17 -0800

    Added splash screen & make things portrait only

---
+++ app/src/main/java/com/jonathanchiou/foodorganizer/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.jonathanchiou.foodorganizer
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+
+class SplashActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        ClientManager.initialize(this)
+        startActivity(Intent(this,
+                             if (ClientManager.get().isAlreadyLoggedIn()) MainActivity::class.java
+                             else LoginActivity::class.java))
+        finish()
+    }
+}
