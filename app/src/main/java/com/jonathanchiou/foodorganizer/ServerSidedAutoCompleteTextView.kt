6d03e0313800e79f63c835c77ca0e605977d7dfa
Jonathan Chiou <jchio799@gmail.com>
2018-11-21 18:16:08 -0800

    Now debouncing text input events for ServerSidedAutoCompleteTextView

--- app/src/main/java/com/jonathanchiou/foodorganizer/ServerSidedAutoCompleteTextView.kt
+++ app/src/main/java/com/jonathanchiou/foodorganizer/ServerSidedAutoCompleteTextView.kt
@@ -8,17 +8,27 @@ import android.util.Log
 import android.widget.AutoCompleteTextView
 import io.reactivex.Observable
 import io.reactivex.Observer
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.functions.Function
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.PublishSubject
+import java.util.concurrent.TimeUnit
+
+sealed class TextEvent
+class TextInputEvent(val query: String) : TextEvent()
+class AutoCompleteEvent: TextEvent()
 
 class ServerSidedAutoCompleteTextView<T>(context: Context, attributeSet: AttributeSet):
         AutoCompleteTextView(context, attributeSet) {
 
     val autoCompleteAdapter = AutoCompleteAdapter<T>()
 
-    protected var previousDisposable : Disposable? = null
+    protected var diposable : Disposable? = null
 
     lateinit var uiModelObservableSupplier : Function<String, Observable<UIModel<List<T>>>>
+
+    val textEventSubject = PublishSubject.create<TextEvent>();
 
     init {
         setAdapter(autoCompleteAdapter)
@@ -29,7 +39,6 @@ class ServerSidedAutoCompleteTextView<T>(context: Context, attributeSet: Attribu
                                            count: Int,
                                            after: Int) {
                 dismissDropDown()
-                previousDisposable?.dispose()
             }
 
             override fun onTextChanged(s: CharSequence?,
@@ -53,40 +62,52 @@ class ServerSidedAutoCompleteTextView<T>(context: Context, attributeSet: Attribu
                 }
 
                 autoCompleteAdapter.reset()
-                uiModelObservableSupplier.apply(editableAsString)
-                        .subscribe(object: Observer<UIModel<List<T>>> {
-                            override fun onSubscribe(d: Disposable) {
-                                previousDisposable?.dispose()
-                                previousDisposable = d
-                            }
-
-                            override fun onNext(uiModel: UIModel<List<T>>) {
-                                Log.i("PlacesAutoComplete", uiModel.state.toString())
-                                if (uiModel.state == State.SUCCESS) {
-                                    autoCompleteAdapter.objects = uiModel.model!!
-                                    autoCompleteAdapter.notifyDataSetChanged()
-                                    showDropDown()
-                                }
-                            }
-
-                            override fun onError(e: Throwable) {
-                                Log.i("PlacesAutoComplete", e.message)
-                            }
-
-                            override fun onComplete() {
-                            }
-                        })
+                textEventSubject.onNext(TextInputEvent(query = editableAsString))
             }
         })
+
+        textEventSubject
+                .debounce(200, TimeUnit.MILLISECONDS)
+                .switchMap {
+                    when (it) {
+                        is TextInputEvent -> uiModelObservableSupplier.apply(it.query)
+                        is AutoCompleteEvent -> Observable.never<UIModel<List<T>>>()
+                                .subscribeOn(Schedulers.io())
+                                .observeOn(AndroidSchedulers.mainThread())
+                    }
+                }
+                .subscribe(object: Observer<UIModel<List<T>>> {
+                    override fun onSubscribe(d: Disposable) {
+                        diposable = d
+                    }
+
+                    override fun onNext(uiModel: UIModel<List<T>>) {
+                        Log.i("AutoComplete", uiModel.state.toString())
+                        if (uiModel.state == State.SUCCESS) {
+                            autoCompleteAdapter.objects = uiModel.model!!
+                            autoCompleteAdapter.notifyDataSetChanged()
+                            showDropDown()
+                        }
+                    }
+
+                    override fun onError(e: Throwable) {
+                        Log.i("AutoComplete", e.message)
+                    }
+
+                    override fun onComplete() {
+                    }
+                })
     }
 
     inline fun doOnItemClicked(crossinline consumer: (T) -> Unit) {
         setOnItemClickListener { _, _, position, _ ->
+            textEventSubject.onNext(AutoCompleteEvent())
             consumer(autoCompleteAdapter.getItem(position))
+            autoCompleteAdapter.reset()
         }
     }
 
     fun cancelPendingRequest() {
-        previousDisposable?.dispose()
+        diposable?.dispose()
     }
 }
