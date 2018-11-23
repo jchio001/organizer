cbca9b5ead557f6f9b7c13f406c1a900203ca9c9
Jonathan Chiou <jchio799@gmail.com>
2018-11-22 20:09:33 -0800

    Implemented core code for picking a time

--- app/src/main/java/com/jonathanchiou/foodorganizer/SchedulerActivity.kt
+++ app/src/main/java/com/jonathanchiou/foodorganizer/SchedulerActivity.kt
@@ -1,15 +1,20 @@
 package com.jonathanchiou.foodorganizer
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
-import android.support.design.chip.Chip
-import android.support.design.chip.ChipGroup
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import butterknife.*
+import android.widget.FrameLayout
+import android.widget.TextView
+import butterknife.BindView
+import butterknife.ButterKnife
+import butterknife.OnCheckedChanged
+import butterknife.OnClick
 import io.reactivex.Observable
 import io.reactivex.functions.Function
+import java.text.SimpleDateFormat
+import java.util.*
 
 class SchedulerActivity : AppCompatActivity() {
 
@@ -22,12 +27,27 @@ class SchedulerActivity : AppCompatActivity() {
     @BindView(R.id.account_chipgroup)
     lateinit var accountChipGroup: ActionChipGroup<Account>
 
-    private var foodOrganizerClient = ClientManager.get().foodOrganizerClient
+    @BindView(R.id.date_layout)
+    lateinit var dateLayout: FrameLayout
+
+    @BindView(R.id.date_textview)
+    lateinit var dateTextView: TextView
+
+    @BindView(R.id.time_textview)
+    lateinit var timeTextView: TextView
+
+    val scheduledTime by lazy {
+        val now = Calendar.getInstance()
+        now.add(Calendar.MINUTE, 30)
+        now
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scheduler)
         ButterKnife.bind(this)
+
+        val foodOrganizerClient = ClientManager.get().foodOrganizerClient
 
         // IGNORE ANDROID STUDIOS. Replacing an interface with a lambda only works if the accepting
         // code is written in Java. It is not.
@@ -62,5 +82,58 @@ class SchedulerActivity : AppCompatActivity() {
         placeAutoCompleteTextView.cancelPendingRequest()
         accountAutoCompleteTextView.cancelPendingRequest()
         finish()
+    }
+
+    @OnCheckedChanged(R.id.now_switch)
+    fun onSwitchStateChanges(isChecked: Boolean) {
+        if (!isChecked) {
+            val scheduledTimeInEpochMs = scheduledTime.time
+            dateTextView.text = DATE_FORMAT.format(scheduledTimeInEpochMs)
+            timeTextView.text = TIME_FORMAT.format(scheduledTimeInEpochMs)
+            dateLayout.visibility = View.VISIBLE
+        } else {
+            dateLayout.visibility = View.GONE
+        }
+    }
+
+    @OnClick(R.id.date_textview)
+    fun onDateTextViewClicked() {
+        DatePickerDialog(this,
+                         { _ , y, m, d ->
+                             scheduledTime.set(Calendar.YEAR, y)
+                             scheduledTime.set(Calendar.MONTH, m)
+                             scheduledTime.set(Calendar.DAY_OF_MONTH, d)
+
+                             dateTextView.text = DATE_FORMAT.format(scheduledTime.time)
+                         },
+                         scheduledTime.get(Calendar.YEAR),
+                         scheduledTime.get(Calendar.MONTH),
+                         scheduledTime.get(Calendar.DAY_OF_MONTH))
+                .show()
+    }
+
+    @OnClick(R.id.time_textview)
+    fun onTimeTextViewClicked() {
+        TimePickerDialog(this,
+                         { _, h, m ->
+                             scheduledTime.set(Calendar.HOUR_OF_DAY, h)
+                             scheduledTime.set(Calendar.MINUTE, m)
+
+                             timeTextView.text = TIME_FORMAT.format(scheduledTime.time)
+                         },
+                         scheduledTime.get(Calendar.HOUR_OF_DAY),
+                         scheduledTime.get(Calendar.MINUTE),
+                         false)
+                .show()
+    }
+
+    companion object {
+        val DATE_FORMAT by lazy {
+            SimpleDateFormat("EEE, MMM, dd, yyyy", Locale.US)
+        }
+
+        val TIME_FORMAT by lazy {
+            SimpleDateFormat("hh:mm aa", Locale.US)
+        }
     }
 }
