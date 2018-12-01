222cae27d65576b1c87e93dc64af9c34d15b49ff
Jonathan Chiou <jchio799@gmail.com>
2018-11-30 16:53:36 -0800

    Cleaned up account code

--- app/src/main/kotlin/com/jonathanchiou/organizer/api/model/Account.kt
+++ app/src/main/kotlin/com/jonathanchiou/organizer/api/model/Account.kt
@@ -3,46 +3,31 @@ package com.jonathanchiou.organizer.api.model
 import com.squareup.moshi.Json
 
 fun createAccount(): Account {
-    val account0 = Account()
-    account0.id = 0
-    account0.firstName = "Erlich"
-    account0.lastName = "Bachman"
-
-    return account0
+    return Account(id = 0,
+                   firstName = "Erlich",
+                   lastName = "Bachman",
+                   profileImage = "")
 }
 
 fun createAccounts(): List<Account> {
-    val account0 = Account()
-    account0.id = 0
-    account0.firstName = "Erlich"
-    account0.lastName = "Bachman"
-
-    val account1 = Account()
-    account1.id = 1
-    account1.firstName = "Jian"
-    account1.lastName = "Yang"
-
-    val account2 = Account()
-    account2.id = 2
-    account2.firstName = "Richard"
-    account2.lastName = "Hendricks"
-
-    return listOf(account0, account1, account2)
+    return listOf(Account(id = 0,
+                          firstName = "Erlich",
+                          lastName = "Bachman",
+                          profileImage = ""),
+                  Account(id = 1,
+                          firstName = "Jian",
+                          lastName = "Yang",
+                          profileImage = ""),
+                  Account(id = 2,
+                          firstName = "Richard",
+                          lastName = "Hendricks",
+                          profileImage = ""))
 }
 
-class Account {
-
-    @Json(name = "id")
-    var id = 0
-
-    @Json(name = "first_name")
-    var firstName = ""
-
-    @Json(name = "last_name")
-    var lastName = ""
-
-    @Json(name = "profile_image")
-    var profileImage = ""
+class Account(@Json(name = "id") val id: Int,
+              @Json(name = "first_name") val firstName: String,
+              @Json(name = "last_name") val lastName: String,
+              @Json(name = "profile_image") val profileImage: String) {
 
     override fun equals(other: Any?): Boolean {
         return if (other == null || other !is Account) false else return id == other.id
--- app/src/main/kotlin/com/jonathanchiou/organizer/api/model/EventBlurb.kt
+++ app/src/main/kotlin/com/jonathanchiou/organizer/api/model/EventBlurb.kt
@@ -4,23 +4,20 @@ import com.jonathanchiou.organizer.main.MainFeedModel
 import com.squareup.moshi.Json
 
 fun createEventBlurbs(): List<EventBlurb> {
-    val account0 = Account()
-    account0.id = 0
-    account0.firstName = "Handsome"
-    account0.lastName = "Jack"
-    account0.profileImage = "https://steamuserimages-a.akamaihd.net/ugc/259343702669198665/F7898569F100F01CB0B48F1A8BD94F87024B03E7/"
+    val account0 = Account(id = 0,
+                           firstName = "Handsome",
+                           lastName = "Jack",
+                           profileImage = "https://steamuserimages-a.akamaihd.net/ugc/259343702669198665/F7898569F100F01CB0B48F1A8BD94F87024B03E7/")
 
-    val account1 = Account()
-    account1.id = 1
-    account1.firstName = "Claptrap"
-    account1.lastName = ""
-    account1.profileImage = "https://i1.wp.com/mentalmars.com/wp-content/uploads/2014/11/claptrap.jpg?resize=250%2C239"
+    val account1 = Account(id = 1,
+                           firstName = "Claptrap",
+                           lastName = "",
+                           profileImage = "https://i1.wp.com/mentalmars.com/wp-content/uploads/2014/11/claptrap.jpg?resize=250%2C239")
 
-    val account2 = Account()
-    account2.id = 2
-    account2.firstName = "MISTER"
-    account2.lastName = "TORGUE"
-    account2.profileImage = "https://pbs.twimg.com/profile_images/2930127149/e18533e68204c0d64c5e22acd2c01248_400x400.jpeg"
+    val account2 = Account(id = 1,
+                           firstName = "MISTER",
+                           lastName = "TORGUE",
+                           profileImage = "https://pbs.twimg.com/profile_images/2930127149/e18533e68204c0d64c5e22acd2c01248_400x400.jpeg")
 
     val date = (System.currentTimeMillis() / 1000) + 86400
 
